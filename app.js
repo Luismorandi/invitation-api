62d00b7229e8a0312c9c2f93500f7d0a3a025fb9
luismo <96397176+Luismorandi@users.noreply.github.com>
2024-03-07 22:27:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const fs = require('fs').promises;
 const path = require('path')
+const cors = require('cors');
 
 const app = express()
 app.use(express.json());
+app.use(cors())
 
 app.listen(3000,()=>{
     console.log("listening on port 3000")
