d9ccdc08fe3684f0808fe46e8c59ffce6d2b46c3
luismo <96397176+Luismorandi@users.noreply.github.com>
2024-03-07 22:02:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const fs = require('fs').promises;
+const path = require('path')
 
 const app = express()
 app.use(express.json());
@@ -8,8 +9,25 @@ app.listen(3000,()=>{
     console.log("listening on port 3000")
 })
 
-app.post('/', async (req, res)=>{
-   console.log(req.body)
-    await fs.appendFile(`./${req.body.name_file}.txt`, `\n ${req.body.message}`, { encoding: 'utf8' });
-    res.status(200).send( req.body)
+app.get('/', async (req, res)=>{
+    const pathData = path.join(__dirname, './data.txt')
+    const texto= await fs.readFile(pathData, 'utf8' );
+    const lineas = texto.split('\n');
+
+    const WORD_DENEGADOS = "denegados";
+    const WORD_ACEPTARON = "aceptaron"
+    
+    const denegados = lineas.filter(cadena => cadena.includes(WORD_DENEGADOS));
+    const aceptados = lineas.filter(cadena => cadena.includes(WORD_ACEPTARON))
+
+    const response = {
+        denegados,
+        aceptados
+    }
+    res.status(200).send( response)
 })
+
+app.post('/', async (req, res)=>{
+     await fs.appendFile(`./data.txt`, `\n ${req.body.status} : ${req.body.message}`, { encoding: 'utf8' });
+     res.status(200).send( req.body)
+ })
